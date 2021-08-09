8b3e3bbecde3270caacc97119dbaa61df670ab76
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-09 17:45:36 +0300

    add react

---
+++ lab01/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Board from './components/Board';
+
+class App extends React.Component {
+  render(){
+    return(
+      <>
+      <h1> my frist react app</h1>
+      <p>hi</p>
+
+      <Board/>
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ lab01/src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Board extends React.Component {
+    render(){
+        return(
+            <div>
+              <div className= 'square'>1</div>  
+              <div className= 'square'>2</div>  
+              <div className= 'square'>3</div>  
+              <div className= 'square'>4</div>  
+              
+            </div>
+        )
+    }
+}
+
+
+export default Board;
